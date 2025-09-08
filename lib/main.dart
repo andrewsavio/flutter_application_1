340fd0bdae97bb42f95d8495cdcc7cfafaca1afd
andrewsavio <saviosquash@gmail.com>
2025-09-08 14:22:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text("Quote App"),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          leading: Icon(Icons.menu),
+        ),
+        drawer: Drawer(child: Center(child: Text("Hello World"))),
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
